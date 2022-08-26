9f663c395545e8ea9eabc2912fdd6fc60c795bb9
ares <58524779+ar3s-xss@users.noreply.github.com>
2022-08-26 09:24:35 +0200

    Update SuperProject.py

--- SuperProject/SuperProject.py
+++ SuperProject/SuperProject.py
@@ -10,6 +10,8 @@ alphabet = list(string.ascii_lowercase)
 #Take all alphabet letters and test if such directory exists
 #If so save dir letter to list and print dir path letter
 
+
+
 for letter in alphabet:
     start_dir = letter
 
@@ -20,7 +22,7 @@ for letter in alphabet:
         target_dir.append(start_dir)
     else:
         print('%s:\\ ----> !!!Does not Exists!!! \n' %(start_dir))
-
+# prints letters of available disks
 for dick in target_dir:
      print("%s \n" %(dick))
         
@@ -31,8 +33,26 @@ for DIR in target_dir:
         if os.path.isdir('%s:\\%s' %(DIR,stuff)):
 
             print('%s:\\%s (Directory)\n' %(DIR.upper(),stuff))
+
+            #check for permisions of directories
+            if os.access('%s:\\%s' %(DIR, stuff), os.R_OK & os.X_OK):
+            
+                try:
+                    for content in os.listdir('%s:\\%s\\' %(DIR, stuff)):
+                        if os.path.isdir('%s:\\%s\\%s' %(DIR, stuff, content)):
+
+                            print("    %s (Directory)" %(content))
+                        else:
+                            print("    %s (File)" %(content))
+
+                except IOError:
+                    print("%s:\\%s\\%s (Directory | No Permissions)" %(DIR, stuff, content))
+
+            else:
+                print("%s:\\%s\\%s (Directory | No Permissions)" %(DIR, stuff, content))
+
         else:
-            print('%s:\\%s (File)\n' %(DIR.upper(),stuff))
+            print("%s:\\%s (File)\n" %(DIR.upper(),stuff))
 
 
 
